de44c30fb17b81d0ef0768c558bd3476a3909043
cndoit18 <cndoit18@outlook.com>
2024-03-23 01:48:22 +0800

    add multi threads

---
+++ crates/webserver/src/lib.rs
@@ -0,0 +1,73 @@
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: Option<mpsc::Sender<Job>>,
+}
+
+impl ThreadPool {
+    pub fn new(size: usize) -> Self {
+        assert!(size > 0);
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+        let mut workers = Vec::with_capacity(size);
+        for id in 0..size {
+            workers.push(Worker::new(id, receiver.clone()))
+        }
+        ThreadPool {
+            workers: workers,
+            sender: Some(sender),
+        }
+    }
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+        self.sender.as_ref().unwrap().send(job).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        drop(self.sender.take());
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+struct Worker {
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
+        let thread = thread::spawn(move || loop {
+            let recv = receiver.lock().unwrap().recv();
+            match recv {
+                Ok(job) => {
+                    println!("Worker {id} got a job; executing.");
+                    job();
+                }
+                Err(_) => {
+                    println!("Worker {id} disconnected; shutting down.");
+                    break;
+                }
+            }
+        });
+        Worker {
+            id,
+            thread: Some(thread),
+        }
+    }
+}
--- crates/webserver/src/main.rs
+++ crates/webserver/src/main.rs
@@ -2,16 +2,23 @@ use std::{
     fs,
     io::{BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
+use webserver::ThreadPool;
 
 const ADDRESS: &'static str = "127.0.0.1:7878";
 fn main() {
     let listener = TcpListener::bind(ADDRESS).unwrap();
-    for stream in listener.incoming() {
+    let pool = ThreadPool::new(4);
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
-        handle_connection(stream);
-        println!("Connection established!");
+        pool.execute(move || {
+            handle_connection(stream);
+            println!("Connection established!");
+        })
     }
+    println!("Shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
@@ -22,21 +29,37 @@ fn handle_connection(mut stream: TcpStream) {
         .take_while(|line| !line.is_empty())
         .collect();
     println!("Request: {:#?}", http_request);
-    if http_request[0] == "GET / HTTP/1.1" {
-        let content = fs::read_to_string("hello.html").unwrap();
-        let length = content.len();
-        stream
-            .write_all(
-                format!("HTTP/1.1 200 OK\r\nContent-Length: {length}\r\n\r\n{content}").as_bytes(),
-            )
-            .unwrap();
-    } else {
-        let contents = fs::read_to_string("404.html").unwrap();
-        let length = contents.len();
+    match http_request[0].as_str() {
+        "GET / HTTP/1.1" => {
+            let content = fs::read_to_string("hello.html").unwrap();
+            let length = content.len();
+            stream
+                .write_all(
+                    format!("HTTP/1.1 200 OK\r\nContent-Length: {length}\r\n\r\n{content}")
+                        .as_bytes(),
+                )
+                .unwrap();
+        }
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            let content = fs::read_to_string("hello.html").unwrap();
+            let length = content.len();
+            stream
+                .write_all(
+                    format!("HTTP/1.1 200 OK\r\nContent-Length: {length}\r\n\r\n{content}")
+                        .as_bytes(),
+                )
+                .unwrap();
+        }
+        _ => {
+            let contents = fs::read_to_string("404.html").unwrap();
+            let length = contents.len();
 
-        let response =
-            format!("HTTP/1.1 404 NOT FOUND\r\nContent-Length: {length}\r\n\r\n{contents}");
+            let response =
+                format!("HTTP/1.1 404 NOT FOUND\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-        stream.write_all(response.as_bytes()).unwrap();
+            stream.write_all(response.as_bytes()).unwrap();
+        }
     }
+    stream.flush().unwrap();
 }
