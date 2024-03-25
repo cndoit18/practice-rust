8df48da6284c0f1ffecc18f2f7fee5aa32c046af
cndoit18 <cndoit18@outlook.com>
2024-03-25 13:55:31 +0800

    add shared state

--- crates/redis/src/main.rs
+++ crates/redis/src/main.rs
@@ -1,34 +1,45 @@
-use mini_redis::{Frame, Connection};
-use tokio::net::{TcpListener, TcpStream};
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
 
+use bytes::Bytes;
+use mini_redis::{Connection, Frame};
+use tokio::net::{TcpListener, TcpStream};
 
 pub const ADDRESS: &'static str = "127.0.0.1:6379";
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind(ADDRESS).await.unwrap();
+    println!("Listening");
+    let db = Arc::new(Mutex::new(HashMap::<String, Bytes>::new()));
     loop {
         let (socket, _) = listener.accept().await.unwrap();
+        let db = db.clone();
+        println!("Accepted");
         tokio::spawn(async move {
-           process(socket).await;
+            process(socket, db).await;
         });
     }
 }
 
-async fn process(socket:TcpStream) {
-    use std::collections::HashMap;
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
+
+async fn process(socket: TcpStream, db: Db) {
     use mini_redis::Command::{self, Get, Set};
-    let mut db = HashMap::new();
     let mut connection = Connection::new(socket);
     while let Some(frame) = connection.read_frame().await.unwrap() {
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
                 // 值被存储为 `Vec<u8>` 的形式
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                db.lock()
+                    .unwrap()
+                    .insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
-                if let Some(value) = db.get(cmd.key()) {
+                if let Some(value) = db.lock().unwrap().get(cmd.key()) {
                     // `Frame::Bulk` 期待数据的类型是 `Bytes`， 该类型会在后面章节讲解，
                     // 此时，你只要知道 `&Vec<u8>` 可以使用 `into()` 方法转换成 `Bytes` 类型
                     Frame::Bulk(value.clone().into())
